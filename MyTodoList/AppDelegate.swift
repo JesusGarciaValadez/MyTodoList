4a24a6d81b465b447cfb482f705cf8f7aecdf784
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-12-02 14:55:49 -0600

    - TODO: Implement local notifications.

--- MyTodoList/AppDelegate.swift
+++ MyTodoList/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+
+    let notificationSettings = UNUserNotificationCenter.current().getNotificationSettings { ( settings ) in
+      print( "Setting: \( settings )" )
+      print( "Settings authorization: \( settings.authorizationStatus )" )
+    }
     return true
   }
 
